9eb6502932ab2a8a75ceaaefde29acf99e0871f2
Thays Peixoto <thabysilva12@gmail.com>
2025-12-08 15:18:25 -0300

    melhora legibilidade dos parametros do construtor

--- src/model/Conta.ts
+++ src/model/Conta.ts
@@ -6,7 +6,11 @@ export class Conta {
     private _titular: string;
     private _saldo: number;
 
-    constructor(numero: number, agencia: number, tipo: number, titular: string, saldo: number) {
+    constructor(numero: number, 
+                agencia: number, 
+                tipo: number, 
+                titular: string, 
+                saldo: number) {
         this._numero = numero;
         this._agencia = agencia;
         this._tipo = tipo;
